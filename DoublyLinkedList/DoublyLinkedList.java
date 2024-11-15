81dac927dfc6304f96e956473e4e6d9eb3e56790
Paritosh <paritosh.mathure99@gmail.com>
2024-11-15 13:57:55 +0530

    Swap pairs method

--- DoublyLinkedList/DoublyLinkedList.java
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -179,4 +179,31 @@ public class DoublyLinkedList {
         length--;
         return temp;
     }
+
+    public void swapPairs(){
+        Node dummyNode = new Node(0);
+        dummyNode.next = head;
+        Node previousNode = dummyNode;
+        while(head!=null && head.next!=null){
+            Node firstNode = head;
+            Node secondNode = head.next;
+            
+            previousNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+            firstNode.prev =secondNode;
+            secondNode.prev = previousNode;
+            
+            if(firstNode.next!=null){
+                firstNode.next.prev = firstNode;
+            }
+            
+            head = firstNode.next;
+            previousNode = firstNode;
+        }
+        head = dummyNode.next;
+        if(head!=null){
+            head.prev = null;
+        }
+    }
 }
